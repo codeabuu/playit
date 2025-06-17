63af2fa7f929c272dde9e69c7e67d086fe3ddf81
codeabuu <abdulngui718@gmail.com>
2025-06-17 19:21:53 -0400

    our story added

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -114,7 +114,7 @@ export const fetchCampaigns = async (): Promise<Campaign[]> => {
       goalAmount: campaign.goal_amount,
       raisedAmount: campaign.total_raised,
       teamNeeds: campaign.team_needs,
-      story: campaign.full_description,
+      story: campaign.our_story,
   }));
   
 };
